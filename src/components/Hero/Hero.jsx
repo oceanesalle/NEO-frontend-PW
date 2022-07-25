ecc9b08a30965f65197813dddcc70cb158e48495
oceansalle <oceanesalleiae@gmail.com>
2022-07-25 19:31:06 +0200

    blur animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,8 @@ import hero_image_back from '../../assets/hero_image_back.png';
 const Hero = () => {
   return (
     <div className="hero">
+
+      <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
        
