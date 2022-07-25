e08f07061ff8b800c036e3498e63e6aafd083c81
oceansalle <oceanesalleiae@gmail.com>
2022-07-25 18:11:40 +0200

    Plans section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 
 
 function App() {
   return (
     <div className="App">
      <Hero />
+     <Plans />
     </div>
   );
 }
